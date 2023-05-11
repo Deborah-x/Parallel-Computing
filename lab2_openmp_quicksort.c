be58bd49e85d0adc647a5cc8bc14f81ec233cd97
xiaoli <2669144437@qq.com>
2023-05-11 15:03:28 +0800

    lab2 quicksort

---
+++ lab2_openmp_quicksort.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+#include "quickSort.h"
+const int n = 10000000;
+int main()
+{
+    omp_set_num_threads(8);
+    double omp_time_sta, omp_time_end;
+    double time_sta, time_end;
+    int *data1, *data2;
+    int num = 8;
+    data1 = (int *)malloc(sizeof(int) * n);
+    data2 = (int *)malloc(sizeof(int) * n);
+    rands(data1, n);
+	rands(data2, n);
+    
+    //并行快速排序
+    omp_time_sta = omp_get_wtime();
+    quickSort_parallel(data1, n, num);
+	omp_time_end = omp_get_wtime();
+    
+    //串行快速排序
+    time_sta = omp_get_wtime();
+    quickSort(data2, 0, n - 1);
+    time_end = omp_get_wtime();
+    
+    //输出运行时间
+    printf("-------------------\n");
+    printf("并行处理时间 : %lf s\n", omp_time_end - omp_time_sta);
+    printf("串行处理时间 : %lf s\n", time_end - time_sta);
+    printf("-------------------\n");
+    printf("\n\n");
+    //输出排序后的数组
+    // printf("-------------------\n");
+    // printf("The final data1 : ");
+    //print(data1, n);
+    // printf("The final data2 : ");
+    // print(data2,n);
+    // printf("-------------------\n");
+    // printf("\n\n");
+    return 0;
+}
---
+++ quickSort.h
@@ -0,0 +1,166 @@
+#include "omp.h"
+#include <stdlib.h>
+#include <stdio.h>
+//随机创建数组
+void rands(int *data,int sum);
+//交换函数
+void sw(int *a, int *b);
+//求2的n次幂
+int exp2_(int wht_num);
+//求log2_(n)
+int log2_(int wht_num);
+//合并两个有序的数组
+void mergeList(int *c,int *a, int sta1, int end1, int *b, int sta2, int end2);
+//串行快速排序
+int partition(int *a, int sta, int end);
+void quickSort(int *a, int sta, int end);
+//openMP(8)并行快速排序
+void quickSort_parallel(int* array, int lenArray, int numThreads);
+void quickSort_parallel_internal(int* array, int left, int right, int cutoff);
+
+void rands(int *data, int sum){
+    int i;
+    for (i = 0; i < sum; i++)
+    {
+        data[i] = rand() % 100000000;
+    }
+}
+
+void sw(int *a, int *b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int exp2_(int wht_num){
+    int wht_i;
+    wht_i=1;
+    while(wht_num>0)
+    {
+        wht_num--;
+        wht_i=wht_i*2;
+    }
+    return wht_i;
+}
+int log2_(int wht_num){
+    int wht_i, wht_j;
+    wht_i=1;
+    wht_j=2;
+    while(wht_j<wht_num)
+    {
+        wht_j=wht_j*2;
+        wht_i++;
+    }
+    if(wht_j>wht_num)
+        wht_i--;
+    return wht_i;
+}
+int partition(int *a, int sta, int end){
+    int i = sta, j = end + 1;
+    int x = a[sta];
+    while (1)
+    {
+        while (a[++i] < x && i < end);
+        while (a[--j] > x);
+        if (i >= j)
+            break;
+        sw(&a[i], &a[j]);
+    }
+    a[sta] = a[j];
+    a[j] = x;
+    return j;
+}
+//并行openMP排序
+void quickSort_parallel(int *a, int lenArray, int numThreads){
+    int cutoff = 1000;
+    #pragma omp parallel num_threads(numThreads) //指定线程数的数量
+    {
+        #pragma omp single //串行执行
+        {
+            quickSort_parallel_internal(a, 0, lenArray - 1, cutoff);
+        }
+    }
+}
+void quickSort_parallel_internal(int *a, int left, int right, int cutoff){
+    int i = left, j = right;
+    int tmp;
+    int pivot = a[(left + right) / 2];
+    //进行数组分割，分成两部分（符合左小右大）
+    while (i <= j)
+    {
+        while (a[i] < pivot)
+            i++;
+        while (a[j] > pivot)
+            j--;
+        if (i <= j){
+            tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    //int j = partition(a,left,right);
+    if (((right - left) < cutoff)){
+        if (left < j){
+            quickSort_parallel_internal(a, left, j, cutoff);
+        }
+        if (i < right){
+            quickSort_parallel_internal(a, i, right, cutoff);
+        }
+    }
+    else{
+        #pragma omp task
+        /*  task是“动态”定义任务的，在运行过程中，
+            只需要使用task就会定义一个任务，
+            任务就会在一个线程上去执行，那么其它的任务就可以并行的执行。
+            可能某一个任务执行了一半的时候，或者甚至要执行完的时候，
+            程序可以去创建第二个任务，任务在一个线程上去执行，一个动态的过程
+        */
+	//对两部分再进行并行的线程排序
+        {
+            quickSort_parallel_internal(a, left, j, cutoff);
+        }
+        #pragma omp task
+        {
+            quickSort_parallel_internal(a, i, right, cutoff);
+        }
+    }
+}
+//合并两个已排序的数组
+void mergeList(int *c,int *a, int sta1, int end1, int *b, int sta2, int end2){
+    int a_index = sta1; // 遍历数组a的下标
+    int b_index = sta2; // 遍历数组b的下标
+    int i = 0;          // 记录当前存储位置
+    //int *c;
+    //c = (int *)malloc(sizeof(int) * (end1 - sta1 + 1 + end2 - sta2 + 1));
+    while (a_index < end1 && b_index < end2){
+        if (a[a_index] <= b[b_index]){
+            c[i] = a[a_index];
+            a_index++;
+        }
+        else{
+            c[i] = b[b_index];
+            b_index++;
+        }
+        i++;
+    }
+    while (a_index < end1){
+        c[i] = a[a_index];
+        i++;
+        a_index++;
+    }
+    while (b_index < end2){
+        c[i] = b[b_index];
+        i++;
+        b_index++;
+    }
+}
+//串行快速排序
+void quickSort(int *a, int sta, int end){
+    if (sta < end){
+        //printf("3\n");
+        int mid = partition(a, sta, end);
+        quickSort(a, sta, mid - 1);
+        quickSort(a, mid + 1, end);
+    }
+}
